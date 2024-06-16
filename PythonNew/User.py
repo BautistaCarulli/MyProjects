5ef6e434197dccad3aa6c126b31419820f7ee7d6
Bautista <160802771+BautistaCarulli@users.noreply.github.com>
2024-06-15 22:45:11 -0300

    Add files via upload

---
+++ PythonNew/User.py
@@ -0,0 +1,12 @@
+user ="BautistaCarulli"
+password = "passwordisprivate"
+
+user_input = input("Introduce your User ID: ")
+password_input = input("Introduce your Password: ")
+
+
+if user != user_input or password != password_input:
+    print("The user is not correct!")
+    quit()
+    
+print("Welcome to the program")
